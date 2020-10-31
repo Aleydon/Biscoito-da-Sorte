7b7fe5c2ff3c3ca434d32d3991974245bd46ee91
Aleydon <aleydonroberto@gmail.com>
2020-10-31 01:13:42 -0300

    Added gradient, style button and phrases

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
 //by Roberto Aleydon
 
 import React, { useState } from 'react';
-import { View, Text, Image, TouchableOpacity } from 'react-native';
+import { View, Text, Image, TouchableOpacity, StatusBar } from 'react-native';
 import { styles } from './styles';
+import { phraseList } from './Constants/consts';
+import LinearGradient from 'react-native-linear-gradient';
+import * as Animatable from 'react-native-animatable';
+
+
+
 
 
 
 function App (){
-    const [ phrase,setPhrase ] = useState('Frase do Biscoito ');
+    const [ phrase,setPhrase ] = useState([])
 
+
+    function generatePhrase(){
+        const randomPhrase = Math.floor(Math.random() * phraseList.length);
+        setPhrase(phraseList[randomPhrase]);
+    }
     
+
     return(
         <>
-            <View style={styles.container}>
-                <Image style={styles.img} source={require('./assets/biscoito.png')}/>
+            <LinearGradient colors={['#81FBB8', '#28C76F']} style={{flex: 1}} > 
+             <StatusBar translucent backgroundColor="transparent" barStyle="dark-content"/>
+                <View style={styles.container}>
+                    <Animatable.Image animation="bounce" style={styles.img} source={require('./assets/biscoito.png')}/>
+                    
+                    <Animatable.View animation="zoomInUp">
+                        <TouchableOpacity style={styles.btnOpen} onPress={generatePhrase}>
+                            <Text style={styles.btnText}>Abrir Biscoito</Text>
+                        </TouchableOpacity>
+                    </Animatable.View>
                 
-                <TouchableOpacity>
-                    <Text>Abrir Biscoito</Text>
-                </TouchableOpacity>
-
-                <Text>{phrase}</Text>
-            </View>
+                    <View style={styles.phraseView}>
+                        <Text style={styles.phraseText}>{phrase}</Text>
+                    </View>
+                </View>
+            </LinearGradient>
         </>
 )}
 
--- src/styles.js
+++ src/styles.js
@@ -15,14 +15,43 @@ export const styles = StyleSheet.create({
     img: {
         width: 200,
         height: 200,
-        marginTop: -100,
-
+        marginTop: -80,
+        marginBottom: 3
     },
 
     phrase: {
         fontStyle: 'italic',
         color: '#dd7b22',
         textAlign: 'center',
+    },
+
+    btnOpen: {
+        margin: 10,
+        width:220,
+        height: 60,
+        backgroundColor: '#377369',
+        borderRadius: 24,
+        marginBottom: 20,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+
+    btnText: {
+        color: '#fff',
+    },
+
+    phraseView:{
+        width: 300,
+        height: 100,
+        marginTop: 20,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+
+    phraseText: {
+        color: 'red',
+        fontWeight: 'bold',
+        fontSize: 15,
     }
 
 });
